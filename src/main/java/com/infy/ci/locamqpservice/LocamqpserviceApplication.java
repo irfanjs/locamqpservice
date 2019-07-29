2d6b26cbc0970a469b1d56fc158906a2e125f647
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-29 12:52:33 +0530

    code

---
+++ src/main/java/com/infy/ci/locamqpservice/LocamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.locamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocamqpserviceApplication.class, args);
+	}
+
+}
